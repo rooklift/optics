ade9b6895d6f0a0a53478b7c676bc03c7dc24dee
rooklift <16438795+rooklift@users.noreply.github.com>
2021-07-02 16:22:44 +0100

    1.2 replay format

--- modules/replay.js
+++ modules/replay.js
@@ -20,21 +20,38 @@ let replay_props = {
 
 	get_units(i) {
 
-		// FIXME / TODO
-
 		let ret = [];
+
+		for (let team_string of Object.keys(this.stateful[i].teamStates)) {
+
+			let team = parseInt(team_string, 10);
+
+			for (let unit_id of Object.keys(this.stateful[i].teamStates[team_string].units)) {
+
+				let foo = this.stateful[i].teamStates[team_string].units[unit_id];
+
+				ret.push({
+					team: team,
+					id: unit_id,
+					type: foo.type,
+					cd: foo.cooldown,
+					x: foo.x,
+					y: foo.y,
+					wood: foo.cargo.wood,
+					coal: foo.cargo.coal,
+					uranium: foo.cargo.uranium
+				});
+			}
+		}
+
 		return ret;
 	},
 
 	get_houses(i) {
-
-		// FIXME / TODO
-
 		let ret = [];
-
 		for (let city of Object.values(this.stateful[i].cities)) {
 			for (let house of city.cityCells) {
-				ret.push({x: house.x, y: house.y, id: city.id, team: 0});			// FIXME
+				ret.push({x: house.x, y: house.y, id: city.id, team: city.team});
 			}
 		}
 		return ret;
